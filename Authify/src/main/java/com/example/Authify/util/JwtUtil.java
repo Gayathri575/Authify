68e7a5a8310697b126a0df29366359358da766d9
Gayathri575 <gayathriii575@gmail.com>
2026-03-06 18:38:05 +0530

    Added filter

--- Authify/src/main/java/com/example/Authify/util/JwtUtil.java
+++ Authify/src/main/java/com/example/Authify/util/JwtUtil.java
@@ -4,7 +4,6 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +11,7 @@ import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class JwtUtil {
@@ -34,5 +34,34 @@ public class JwtUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + 1000*60*60*10))
                 .signWith(key)
                 .compact();
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims , T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String extractEmail(String token){
+        return extractClaim(token , Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token){
+        return extractClaim(token , Claims::getExpiration);
+    }
+
+    public Boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Boolean validateToken(String token , UserDetails userDetails){
+        final String email = extractEmail(token);
+        return (email.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 }
